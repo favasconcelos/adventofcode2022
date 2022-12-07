69c89b765681faa08422d33cd7de32ce5644ce4f
Felipe Vasconcelos <f@avasconcelos.com>
2022-12-07 19:31:52 +0100

    feat: day-6

---
+++ day-6/q1.js
@@ -0,0 +1,31 @@
+const { readFile } = require("../utils/input");
+
+async function main() {
+  let indexFound = false;
+
+  function checkLetters(marker = "") {
+    // console.log("checkLetters", { marker });
+    for (let i = 0; i < marker.length; i++) {
+      for (let j = i + 1; j < marker.length; j++) {
+        if (marker[i] === marker[j]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  function handleLine(line = "") {
+    for (let index = 4; index < line.length - 1; index++) {
+      const possibleMarker = line.substring(index - 4, index);
+      if (checkLetters(possibleMarker)) {
+        indexFound = index;
+        break;
+      }
+    }
+  }
+
+  await readFile(__dirname, "input.txt", handleLine);
+  console.log({ indexFound });
+}
+main();
